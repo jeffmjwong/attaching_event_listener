d80153d0c0f6eb556bea278be8a04ed8c1fb4bcc
Jeff Wong <jeffmjwong@gmail.com>
2018-03-09 20:43:03 +1000

    Event delegation and event target object

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
+const unorderedList = document.getElementsByTagName('ul')[0];
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
@@ -12,9 +13,9 @@ toggleList.addEventListener('click', () => {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -29,16 +30,21 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
+
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
-});  
-  
-  
-  
-  
-  
-  
-  
+});
+
+unorderedList.addEventListener('mouseover', (evt) => {
+  if (evt.target.tagName === "LI") {
+    evt.target.textContent = evt.target.textContent.toUpperCase();
+  };
+});
+
+unorderedList.addEventListener('mouseout', (evt) => {
+  if (evt.target.tagName === "LI") {
+    evt.target.textContent = evt.target.textContent.toLowerCase();
+  };
+});
